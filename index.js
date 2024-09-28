d141333b2c691c53ab3e5832f0fa206490782691
Himanshu Rawat <himanshuu004@gmail.com>
2024-09-28 12:33:22 +0530

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-var btn = document.querySelector("button");
+var block = document.querySelector(".btn1");
+var unblock = document.querySelector(".btn2");
 
 
 
-function getNewUSer(){
-  fetch(`https://randomuser.me/api/`)
-.then(raw => raw.json())
-.then(result =>{
-  const{name,email,gender,picture}=result.results[0];
-
-  document.querySelector(".cards").innerHTML +=`
-        <div class="card w-56 p-4  rounded-xl bg-zinc-600">
-        <div class="w-32 h-32 mb-2  bg-white rounded-full overflow-hidden">
-          <img  src="${picture.large}" class="w-full h-full fit-cover" alt="user photo">
-        </div>
-        <h3 class="font-semibold text-2xl">${name.first}</h3>
-        <h4 class="font-semibold text-sm opacity-90">${gender}</h4>
-        <h5 class="font-semibold text-sm opacity-60">${email}</h5>
-        <p class="mt-1.5 text-xs font-semibold opacity-80">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-      </div>`
-
+block.addEventListener("click",function(){
+  localStorage.setItem("block","true");
+  disp();
 });
+
+unblock.addEventListener("click",function(){
+  localStorage.setItem("block","false");
+  disp();
+});
+
+function disp(){
+  if(localStorage.getItem("block") == "true"){
+    document.querySelector("#status").textContent = "BLOCKED";
+  }
+  else{
+    document.querySelector("#status").textContent = "UNBLOCKED";
+  }
 }
 
-btn.addEventListener("click",function(){
-  getNewUSer();
-})
+disp();
+
